1430e63715cc58cd77556f06ed7c0ac3d70fcc71
Po Cheng Lo <zxc45544@gmail.com>
2024-07-12 11:13:08 +0800

    新增商品列表檔案連結php檔，新增.gitignore

---
+++ popo/db-connect-setting.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/setting.php';
+
+$dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
+
+$pdo_options = [
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+$pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+
+try{
+  $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+} catch (PDOException $ex){
+  echo 'Connection failed', $ex->getMessage();
+}
