442e00cf41ca71dd98640314309b5a5875e48c88
Po Cheng Lo <zxc45544@gmail.com>
2024-07-15 09:02:36 +0800

    新增登入頁面

--- popo/db-connect-setting.php
+++ popo/db-connect-setting.php
@@ -9,10 +9,15 @@ $pdo_options = [
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 ];
 
-$pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+// $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
 
 try{
   $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
 } catch (PDOException $ex){
   echo 'Connection failed', $ex->getMessage();
+}
+
+if(!isset($_SESSION)){
+  //如果沒有啟動 session 的功能，就啟動
+  session_start();
 }
