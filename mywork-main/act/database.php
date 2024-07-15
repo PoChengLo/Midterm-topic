14571924c7d8b069acf67e9ad3ea9c822a2f186c
Wick00502 <dollz005522@gmail.com>
2024-07-15 16:10:16 +0800

    修正查詢換頁條件消失&資料庫調整

---
+++ mywork-main/act/database.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root"; // 默認的XAMPP使用者名稱
+$password = "a00580308"; // 默認的XAMPP密碼為空
+$dbname = "activities"; // 你的資料庫名稱
+
+// 建立連接
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 檢查連接
+if ($conn->connect_error) {
+  die("連接失敗: " . $conn->connect_error);
+}
