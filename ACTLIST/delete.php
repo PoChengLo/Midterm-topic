94d3b05fded4bb4e7f9a6f9c639ecacc6514d507
Wick00502 <dollz005522@gmail.com>
2024-07-17 10:57:19 +0800

    v4 	 API更新修正頁面查詢

---
+++ ACTLIST/delete.php
@@ -0,0 +1,25 @@
+<?php
+include 'database.php';
+
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+
+  // 刪除 actlist 表中的活動
+  $sql_actlist = "DELETE FROM actlist WHERE act_id='$id'";
+  if ($conn->query($sql_actlist) === TRUE) {
+    $message = "刪除成功";
+  } else {
+    $message = "錯誤: " . $conn->error;
+  }
+} else {
+  $message = "錯誤: " . $conn->error;
+}
+
+// 關閉資料庫連接
+$conn->close();
+
+// 使用JavaScript顯示通知並返回到主頁
+echo "<script>
+            alert('$message');
+            window.location.href = 'index_act.php';
+          </script>";
