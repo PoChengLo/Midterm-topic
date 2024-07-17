5cbda238e41fae061bfccf756ad37da59a13a8b0
Leeying8413 <PlayUncle1925@gmail.com>
2024-07-17 21:48:55 +0800

    USER_PROJECT 修改丟臉的錯字

--- user_project/user_CRUD/parts/html-head.php
+++ user_project/user_CRUD/parts/html-head.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title><?= empty($title) ? '密室逃脫桌游網' : "$title - 密室逃脫桌游網" ?></title>
+  <title><?= empty($title) ? '密室逃脫桌遊網' : "$title - 密室逃脫桌遊網" ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
