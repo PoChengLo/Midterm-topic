b7f197cf25cfd3ec5c76adaefce770a6fc040cab
Po Cheng Lo <zxc45544@gmail.com>
2024-07-19 20:44:21 +0800

    新增測試帳號

--- ACTLIST/database.php
+++ ACTLIST/database.php
@@ -1,8 +1,14 @@
 <?php
-$servername = "localhost";
+// $servername = "localhost";
+// $username = "root"; // 默認的XAMPP使用者名稱
+// $password = "a00580308"; // 默認的XAMPP密碼為空
+// $dbname = "activities"; // 你的資料庫名稱
+
+$servername = "127.0.0.1";
 $username = "root"; // 默認的XAMPP使用者名稱
-$password = "a00580308"; // 默認的XAMPP密碼為空
-$dbname = "activities"; // 你的資料庫名稱
+$password = ""; // 默認的XAMPP密碼為空
+$dbname = "new_db_0718"; // 你的資料庫名稱
+
 
 // 建立連接
 $conn = new mysqli($servername, $username, $password, $dbname);
