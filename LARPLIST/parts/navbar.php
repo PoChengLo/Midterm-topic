c56fe2db071b77a6e27d1546e8eb1005dc10d82e
Zizi <rongnong2324@gmail.com>
2024-07-22 16:26:46 +0800

    新增navbar logo

--- LARPLIST/parts/navbar.php
+++ LARPLIST/parts/navbar.php
@@ -36,7 +36,7 @@ if (!isset($pageName)) {
 <div class="container-fill mb-5 vw-100 position-fixed z-1 top-0">
   <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index_.php">Navbar</a>
+      <a class="navbar-brand" href="index_larp.php"><img src="../user_project/user_CRUD/img/Design_sm.png" alt=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
